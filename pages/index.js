010d8cf842156541e8ac0eb9edb1f8a83003131b
Renato Rocha Rodrigues <renatorrodrigues2002@gmail.com>
2022-01-24 13:21:37 -0300

    Changes the background to a gif

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function PaginaInicial() {
           justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
           backgroundImage:
-            'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            'url(https://c.tenor.com/KjVxfRrrncUAAAAd/matrix.gif)',
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
           backgroundBlendMode: 'multiply'
